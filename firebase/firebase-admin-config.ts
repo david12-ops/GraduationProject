ca62b835333a46d40a845da8d559c81fbf28803c
david12-ops <77929483+david12-ops@users.noreply.github.com>
2024-03-24 02:41:35 +0100

    update_24_03

--- firebase/firebase-admin-config.ts
+++ firebase/firebase-admin-config.ts
@@ -1,6 +1,19 @@
 import * as admin from 'firebase-admin';
 
-const firebaseAdminConfig = process.env.NEXT_PUBLIC_FIREBASE_CONFIG;
+const firebaseAdminConfig = {
+  type: process.env.NEXT_PUBLIC_TYPE,
+  project_id: process.env.NEXT_PUBLIC_PROJEKT_ID,
+  private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
+  private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY?.replace(/\\n/gm, '\n'),
+  client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+  client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+  auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
+  token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.NEXT_PUBLIC_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.NEXT_PUBLIC_UNIVERSE_DOMAIN,
+};
 
 if (admin.apps.length === 0) {
   // Initialize Firebase
