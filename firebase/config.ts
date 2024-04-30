d78a87cc460f57b4e7ec54ac27db7e17cedbd8a7
david12-ops <77929483+david12-ops@users.noreply.github.com>
2024-04-30 13:55:00 +0200

    update_30_04

--- firebase/config.ts
+++ firebase/config.ts
@@ -11,7 +11,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebase_app =
+const firebaseApp =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-export default firebase_app;
+// eslint-disable-next-line import/no-default-export
+export default firebaseApp;
